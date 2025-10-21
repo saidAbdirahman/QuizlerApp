76d1b60a82dcf08fdc3489b01fbf482683bf6a3e
saidAbirahman <saidqaaje11@gmail.com>
2025-10-21 23:58:59 +0300

    Add state and make true button work
    Signed-off-by: saidAbirahman <saidqaaje11@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -89,6 +89,14 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+               setState(() {
+                 scoreKeeper.add(
+                     Icon(
+                       Icons.check,
+                       color: Colors.green,
+                     )
+                 );
+               });
               },
             ),
           ),
