65be16ce5a60084b44f683dd88dabd416a617ee3
saidAbirahman <saidqaaje11@gmail.com>
2025-10-21 23:33:05 +0300

    Add Empty List
    Signed-off-by: saidAbirahman <saidqaaje11@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class Quizzler extends StatelessWidget {
 }
 
 class QuizPage extends StatefulWidget {
+  List scoreKeeper = [];
   @override
   _QuizPageState createState() => _QuizPageState();
 }
