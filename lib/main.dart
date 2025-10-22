6c46e15274653c2a9b8f46d44674b4251608b8e7
saidAbirahman <saidqaaje11@gmail.com>
2025-10-22 09:41:11 +0300

    Add Questions List
    Signed-off-by: saidAbirahman <saidqaaje11@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,11 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
+  List<String> questions = [
+  'You can lead a cow down stairs but not up stairs.',
+  'Approximately one quarter of human bones are in the feet.',
+  'A slug\'s blood is green.'
+  ];
   @override
   Widget build(BuildContext context) {
     return Column(
