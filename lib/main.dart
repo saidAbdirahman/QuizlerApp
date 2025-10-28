9b62ba919e0d8bd23ee7c4d9b8ee03d3f5d15ed8
saidAbirahman <saidqaaje11@gmail.com>
2025-10-28 21:28:05 +0300

    Applied to second button

--- lib/main.dart
+++ lib/main.dart
@@ -109,7 +109,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
-                bool correctAnswer = answers[tracker];
+                bool correctAnswer = questionBank[tracker].questionAnswer;
                 if(correctAnswer == false){
                   print('User got it right');
                 }else{
