dbffc5ea9d0e03dea141cab13bd2dc90716a9942
saidAbirahman <saidqaaje11@gmail.com>
2025-10-28 21:25:22 +0300

    Remove the sample object for testing

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,9 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  Question q1 = Question(q: 'You can lead a cow down stairs but not up stairs.', a: false);
-  List<Question> Questions = [
+
+  
+  List<Question> questionBank = [
     Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
     Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
     Question(q: 'A slug\'s blood is green.', a: true)
