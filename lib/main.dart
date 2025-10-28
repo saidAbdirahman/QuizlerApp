e41950c032f8ae6ac5a40bc169fa75ea4c5e98d4
saidAbirahman <saidqaaje11@gmail.com>
2025-10-28 21:28:45 +0300

    Remove unwanted space
    Signed-off-by: saidAbirahman <saidqaaje11@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -28,13 +28,10 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-
-  
   List<Question> questionBank = [
     Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
     Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
     Question(q: 'A slug\'s blood is green.', a: true)
-
   ];
 
   int tracker = 0;
