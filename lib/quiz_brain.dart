f8ebe6e4fe4013fdaeb4fb72542028ea4bdd5c27
saidAbirahman <saidqaaje11@gmail.com>
2025-11-01 21:49:55 +0300

    Add List of questions

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -1,3 +1,8 @@
 class QuizBrain{
-  
+  List<Question> questionBank = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
+    Question(q: 'A slug\'s blood is green.', a: true)
+  ];
+
 }
