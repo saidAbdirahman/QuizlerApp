6b58cc5eb9df7f98e3e967b708bd42664f7d04bf
saidAbirahman <saidqaaje11@gmail.com>
2025-11-01 22:09:00 +0300

    Update here same same thing

--- lib/main.dart
+++ lib/main.dart
@@ -74,7 +74,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
-                bool correctAnswer = questionBank[tracker].questionAnswer;
+                bool correctAnswer = quizBrain.questionBank[tracker].questionAnswer;
                 if(correctAnswer == true){
                   print('User got it right');
                 }else{
