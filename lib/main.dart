71600d2f0ccdcbb17f59573c6f66e24bb0149f01
saidAbirahman <saidqaaje11@gmail.com>
2025-11-01 22:38:07 +0300

    Call the function and pass it the tracker number

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                quizBrain.questionBank[tracker].questionText,
+                quizBrain.getQuestionText(tracker),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
