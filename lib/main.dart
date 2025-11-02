bbf80c48be479618c6fc71732dc78d60226d4f5b
saidAbirahman <saidqaaje11@gmail.com>
2025-11-02 14:14:05 +0300

    Call the method and pass it false argument

--- lib/main.dart
+++ lib/main.dart
@@ -108,6 +108,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                checkAnswer(false)
 
               },
             ),
