2dcb8b303636d7b7492ead8d46ff71c873d2619b
saidAbirahman <saidqaaje11@gmail.com>
2025-11-02 14:13:23 +0300

    Call the method and pass it to true

--- lib/main.dart
+++ lib/main.dart
@@ -86,6 +86,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                checkAnswer(true)
 
               },
             ),
